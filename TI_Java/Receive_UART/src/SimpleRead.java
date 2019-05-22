95af55535b60504a73f0b816fbb08760cd135011
Sonja <1813090@stud.hs-mannheim.de>
2019-05-22 06:26:18 +0200

    added Java Script to receive UART Data from Radar Sensor; no sending to network yet

--- TI_Java/Receive_UART/src/SimpleRead.java
+++ TI_Java/Receive_UART/src/SimpleRead.java
@@ -3,78 +3,163 @@
 import java.io.*;
 import java.util.*;
 import gnu.io.*;
+import java.net.*;
 
-public class SimpleRead implements Runnable, SerialPortEventListener {
+public class SimpleRead implements Runnable, SerialPortEventListener 
+{
     static CommPortIdentifier portId;
     static Enumeration portList;
+    
+    public static String sensor_output;
 
     InputStream inputStream;
     SerialPort serialPort;
     Thread readThread;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException, IOException 
+    {
         portList = CommPortIdentifier.getPortIdentifiers();
 
-        while (portList.hasMoreElements()) {
+        while (portList.hasMoreElements()) 
+        {
             portId = (CommPortIdentifier) portList.nextElement();
-            if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
-                 if (portId.getName().equals("COM1")) {
+            if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL) 
+            {
+                 if (portId.getName().equals("COM8")) 
+                 {
 			//                if (portId.getName().equals("/dev/term/a")) {
                     SimpleRead reader = new SimpleRead();
-                }
+            		
+                    String hostname = "192.168.137.30";
+            	    int port = Integer.parseInt("2016");
+            	    
+            	    Socket socket = new Socket(hostname, port);
+            	    OutputStream output = socket.getOutputStream();
+            	    PrintWriter writer = new PrintWriter(output, true);
+            	    writer.println("Radar Sensor connected");            	    	
+                 }
             }
         }
     }
 
-    public SimpleRead() {
-        try {
+    public SimpleRead() throws UnknownHostException, IOException 
+    {
+    	//Network_connection network_connect = new Network_connection();    	
+    	/*
+    	int connected_to_server =  network_connection();
+    	if (connected_to_server == 1 )
+    	{
+    		System.out.println("Connected to server");
+    	}
+    	else
+    	{
+    		System.out.println("Not connected to server");
+    	}
+    	*/
+        try 
+        {
             serialPort = (SerialPort) portId.open("SimpleReadApp", 2000);
-        } catch (PortInUseException e) {System.out.println(e);}
-        try {
+        } 
+        catch (PortInUseException e) 
+        {
+        	System.out.println(e);
+        }
+        try 
+        {
             inputStream = serialPort.getInputStream();
-        } catch (IOException e) {System.out.println(e);}
-	try {
-            serialPort.addEventListener(this);
-	} catch (TooManyListenersException e) {System.out.println(e);}
+        } 
+        catch (IOException e) 
+        {
+        	System.out.println(e);
+        }
+	try 
+	{
+		serialPort.addEventListener(this);
+	} 
+	catch (TooManyListenersException e) 
+	{
+		System.out.println(e);
+	}
         serialPort.notifyOnDataAvailable(true);
-        try {
-            serialPort.setSerialPortParams(9600,
+        try 
+        {
+            serialPort.setSerialPortParams(921600,
                 SerialPort.DATABITS_8,
                 SerialPort.STOPBITS_1,
                 SerialPort.PARITY_NONE);
-        } catch (UnsupportedCommOperationException e) {System.out.println(e);}
+        } 
+        catch (UnsupportedCommOperationException e) 
+        {
+        	System.out.println(e);
+        }
         readThread = new Thread(this);
         readThread.start();
     }
 
-    public void run() {
-        try {
+    public void run() 
+    {
+        try 
+        {
             Thread.sleep(20000);
-        } catch (InterruptedException e) {System.out.println(e);}
-    }
-
-    public void serialEvent(SerialPortEvent event) {
-        switch(event.getEventType()) {
-        case SerialPortEvent.BI:
-        case SerialPortEvent.OE:
-        case SerialPortEvent.FE:
-        case SerialPortEvent.PE:
-        case SerialPortEvent.CD:
-        case SerialPortEvent.CTS:
-        case SerialPortEvent.DSR:
-        case SerialPortEvent.RI:
-        case SerialPortEvent.OUTPUT_BUFFER_EMPTY:
-            break;
-        case SerialPortEvent.DATA_AVAILABLE:
-            byte[] readBuffer = new byte[20];
-
-            try {
-                while (inputStream.available() > 0) {
-                    int numBytes = inputStream.read(readBuffer);
-                }
-                System.out.print(new String(readBuffer));
-            } catch (IOException e) {System.out.println(e);}
-            break;
+        } 
+        catch (InterruptedException e) 
+        {
+        	System.out.println(e);
         }
     }
+
+    public void serialEvent(SerialPortEvent event) 
+    {	    
+        switch(event.getEventType()) 
+        {
+	        case SerialPortEvent.BI:
+	        case SerialPortEvent.OE:
+	        case SerialPortEvent.FE:
+	        case SerialPortEvent.PE:
+	        case SerialPortEvent.CD:
+	        case SerialPortEvent.CTS:
+	        case SerialPortEvent.DSR:
+	        case SerialPortEvent.RI:
+	        case SerialPortEvent.OUTPUT_BUFFER_EMPTY:
+	            break;
+	        case SerialPortEvent.DATA_AVAILABLE:
+	            byte[] readBuffer = new byte[20];
+	
+	            try 
+	            {
+	                while (inputStream.available() > 0) 
+	                {
+	                    int numBytes = inputStream.read(readBuffer);
+	                }
+	                System.out.println(new String(readBuffer));
+	                provide_sensor_output(new String(readBuffer));
+	            } 
+	            catch (IOException e) 
+	            {
+	            	System.out.println(e);
+	            }
+	            break;
+        }
+    }
+    
+    public int provide_sensor_output(String output)
+    {
+    	System.out.println("Function called " + output);
+    	if(output == "R")
+    	{
+    		//System.out.println("Function called; 1");
+    		return 1;
+    	}
+    	else if(output == "U")
+    	{
+    		//System.out.println("Function called; 2");
+    		return 2;
+    	}
+    	else
+    	{
+    		//System.out.println("Function called; 0");
+    		return 0;
+    	}
+    }
 }
+
--- TI_Java/Receive_UART/src/UART_communication.java
+++ TI_Java/Receive_UART/src/UART_communication.java
@@ -1,34 +1,109 @@
 // https://github.com/muzkat/serial-communication-java/blob/master/src/FirstSteps.java
+// https://en.wikibooks.org/wiki/Serial_Programming/Serial_Java
 
 import gnu.io.*; // RXTX
 import java.util.Enumeration;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.*;
 
 public class UART_communication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, UnsupportedCommOperationException {
 		System.out.println("Program started");
 		
 		//System.out.println(java.library.path);
 	    CommPortIdentifier serialPortId;
 	    //static CommPortIdentifier sSerialPortId;
-	    Enumeration enumComm;
+	    Enumeration portIdentifier;
 	    //SerialPort serialPort;
 
-	    enumComm = CommPortIdentifier.getPortIdentifiers();
-	    while (enumComm.hasMoreElements()) {
-	     	serialPortId = (CommPortIdentifier) enumComm.nextElement();
-	     	if(serialPortId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+	    portIdentifier = CommPortIdentifier.getPortIdentifiers();
+	    
+	    CommPortIdentifier portID = null; //will only be set, if port is found
+	    //String[] portIDs = {};
+	    //CommPortIdentifier[] portIDs = {};
+	    //int counter = 0;
+	    
+	    while (portIdentifier.hasMoreElements()) 
+	    {
+	     	serialPortId = (CommPortIdentifier) portIdentifier.nextElement();
+	     	if(serialPortId.getPortType() == CommPortIdentifier.PORT_SERIAL)
+	     	{
 	    		System.out.println(serialPortId.getName());
+	    		portID = serialPortId;
+	    		//portIDs[counter] = portID.getName();
+	    		//counter++;
 	    	}
 	     	//if(serialPortId.getName().equals("COM8"))
 	     	//{
 	     	//	System.out.println("found");
 	     	//}
 	    }
-
+	    
+	    if (portID == null)
+	    {
+	    	System.err.println("Could not find any serial ports");
+	    }
+	    else
+	    {
+	    	Scanner scan = new Scanner(System.in);
+		    System.out.print("Please enter port name that should be opened: ");
+		    String selectedPortName = scan.nextLine();
+		    //System.out.println(selectedPortName);
+		    
+		    SerialPort port = null;
+		    try
+		    {
+		    	port = (SerialPort)portID.open("name",1000);
+		    	System.out.println("Selected Port opened");
+//		    	
+		    	port.setSerialPortParams(
+		    							921600, 
+		    							SerialPort.DATABITS_8,
+		    							SerialPort.STOPBITS_1,
+		    							SerialPort.PARITY_NONE);
+		    	BufferedReader is = null;
+		    	PrintStream os = null;
+		    	
+		    	try {
+		    		is = new BufferedReader(new InputStreamReader(port.getInputStream()));
+		    		while(true)
+		    		{
+		    			String response = is.readLine();
+		    			System.out.println(response);
+		    		}
+		    	}
+		    	catch(IOException e)
+		    	{
+		    		System.err.println("Can't open input stream " + e);
+		    		is = null;
+		    	}
+//		    	for(int i = 0; i < portIDs.length; i++)
+//		    	{
+//		    		if(portID.getName() == selectedPortName)
+//		    		{
+//		    			port = (SerialPort)portID.open("name",1000);
+//		    			System.out.println("Selected Port opened");
+//		    		}
+//		    	}
+		    }
+		    
+		    catch(PortInUseException e)
+		    {
+		    	System.err.println("Port already in use " + e);
+		    	System.exit(1);
+		    }
+	    }
+//	    if(is != null) is.close();
+//	    if(os != null) os.close();
+//	    if(port != null) port.close();
+	    
 		System.out.println("Finished successfully");
+		System.exit(1);
 	}
 }
